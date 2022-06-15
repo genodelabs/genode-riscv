22114bbffb9367ad9668ea9ec18c54a8bc660bae
Piotr Tworek <tworaz@tworaz.net>
2022-06-15 23:11:56 +0200

    riscv_qemu: Initalize PLIC interrupt priorities.
    When using default Qemu 7.x build PLIC interrupts do not seem to work.
    I'm not sure if this is caused by Qemu itself, or newer OpenSBI version.
    Still, the actual root cause is pretty simple. Genode does not try to
    initialize PLIC priority threshold and interrupt priorities. As result
    the threshold is by default set to value 7, while individual irq
    priorities are 0. IRQ priority of 0 means that the interrupt is disabled
    according to PLIC 1.0-rc2 specification.
    
    Fix this by setting interrupt threshould to 0 and interrupt priorities
    to 1.
    
    issue #2

--- src/core/board/riscv_qemu/plic.h
+++ src/core/board/riscv_qemu/plic.h
@@ -18,14 +18,42 @@ namespace Board { class Plic; }
 
 struct Board::Plic : Genode::Mmio
 {
-		enum { NR_OF_IRQ = 32 };
+		enum {
+			/*
+			 * Single core Qemu virt machine uses context 1 for
+			 * supervisor mode interrupts and 0 for machine mode.
+			 */
+			CONTEXT   = 0x1,
+			/*
+			 * VIRT_IRQCHIP_NUM_SOURCES from Qemu include/hw/riscv/virt.h.
+			 */
+			NR_OF_IRQ = 96,
+		};
+		enum {
+			ENABLE_BASE    = 0x2000,
+			ENABLE_STRIDE  = 0x80,
+			CONTEXT_BASE   = 0x200000,
+			CONTEXT_STRIDE = 0x1000,
+		};
+		enum {
+			ENABLE_ADDR   = ENABLE_BASE + CONTEXT * ENABLE_STRIDE,
+			PRI_THR_ADDR  = CONTEXT_BASE + CONTEXT * CONTEXT_STRIDE,
+			ID_ADDR       = CONTEXT_BASE + CONTEXT * CONTEXT_STRIDE + 0x4,
+		};
 
-		struct Enable : Register_array<0x80, 32, 32, 1> { };
-		struct Id     : Register<0x1ff004, 32> { };
+		struct Priority           : Register_array<0x4, 32, NR_OF_IRQ - 1, 32> { };
+		struct Enable             : Register_array<ENABLE_ADDR, 32, NR_OF_IRQ, 1> { };
+		struct Priority_threshold : Register<PRI_THR_ADDR, 32> { };
+		struct Id                 : Register<ID_ADDR, 32> { };
 
 		Plic(Genode::addr_t const base)
 		:
-			Mmio(base) { }
+			Mmio(base)
+		{
+			write<Priority_threshold>(0);
+			for (unsigned i = 0; i < NR_OF_IRQ - 1; ++i)
+				write<Priority>(1, i);
+		}
 
 		void enable(unsigned value, unsigned irq)
 		{
